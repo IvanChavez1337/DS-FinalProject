a373a93869ce9d91cce699cc0c69552bce39f6e2
Iván Chávez <32714261+IvanChavez1337@users.noreply.github.com>
2020-12-07 16:30:48 -0600

    Metodo de consultap por pais listo

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -12,6 +12,16 @@ export class HomeComponent implements OnInit {
     global: any;
     recuperados: number;
     opcion: any;
+
+      //Variables consultas por pais
+    nombrepais: any;
+    casostotales:any;
+    muertestotales:any;
+    rectotales:any;
+    nuevosconf:any;
+    nuevasmuertes:any;
+    nuevosrec:any;
+
   constructor(private dataProviders: DataProvidersService) {
    this.dataProviders.getCovidData().subscribe((res: any) => {console.log(res)
     this.paises=res['Countries'];
@@ -20,6 +30,21 @@ export class HomeComponent implements OnInit {
     this.recuperados = this.global['TotalRecovered']; //Evitamos error de tipo en consola
     console.log(this.global);
   });
+   }
+
+   consultar(){
+    if(this.opcion!=null||this.opcion!=undefined){
+      console.log(this.opcion);
+      this.nombrepais = this.opcion.Country;
+      this.casostotales = this.opcion.TotalConfirmed;
+      this.muertestotales = this.opcion.TotalDeaths;
+      this.rectotales = this.opcion.TotalRecovered;
+      this.nuevosconf = this.opcion.NewConfirmed;
+      this.nuevasmuertes = this.opcion.NewDeaths;
+      this.nuevosrec = this.opcion.NewRecovered;
+      
+    }
+     
    }
 
   ngOnInit(): void {
