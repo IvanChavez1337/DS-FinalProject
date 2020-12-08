1322e651644e654d5a703966e71752bc5562cb57
Iván Chávez <32714261+IvanChavez1337@users.noreply.github.com>
2020-12-08 03:15:51 -0600

    Select de consulta historica lista
    Ya se hace correctamente la consulta historica proveniente de la base de datos.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -17,23 +17,10 @@ export class HomeComponent implements OnInit {
   fecha: Date;
   fecha_aux: string;
   registroglob:any = {};
-      //Variables consultas por pais
-    nombrepais: any;
-    casostotales:any;
-    muertestotales:any;
-    rectotales:any;
-    nuevosconf:any;
-    nuevasmuertes:any;
-    nuevosrec:any;
 
-    //Variables Datos Globales
-    g_casostotales:any;
-    g_muertestotales:any;
-    g_rectotales:any;
-    g_nuevosconf:any;
-    g_nuevasmuertes:any;
-    g_nuevosrec:any;
-
+    //Variables datos historicos
+      datos: any[] =[];
+      opcionHistorica: any;
 
 
   constructor(private dataProviders: DataProvidersService, public dbservice:DbService) {
@@ -46,7 +33,7 @@ export class HomeComponent implements OnInit {
     console.log(this.global);
     this.fecha_aux = moment(this.fecha).format('YYYY-MM-DD');
     console.log("fecha auxiliar "+this.fecha_aux);
-
+  
     
     this.registroglob['casostotales'] = this.global.TotalConfirmed;
     this.registroglob['muertestotales'] = this.global.TotalDeaths;
@@ -55,49 +42,51 @@ export class HomeComponent implements OnInit {
     this.registroglob['nuevasmuertes'] = this.global.NewDeaths;
     this.registroglob['nuevosrec'] = this.global.NewRecovered;
     this.registroglob['fecha'] = this.fecha_aux;
+ 
     this.dbservice.insertarRegGlob(this.registroglob,this.fecha_aux).then(res => {
       console.log("Registro global insertado");
     }).catch(error =>{
       console.log(error);
     })
   this.dbservice.fireservice.collection('paises').valueChanges().subscribe(val => {
-    console.log(val);
+      this.datos = val;
+     console.log(val);
   });
-  });
+});
+  
    }
 
-   consultar(){
+   insertar(){
     if(this.opcion!=null||this.opcion!=undefined){
       console.log(this.opcion);
-      this.nombrepais = this.opcion.Country;
-      this.casostotales = this.opcion.TotalConfirmed;
-      this.muertestotales = this.opcion.TotalDeaths;
-      this.rectotales = this.opcion.TotalRecovered;
-      this.nuevosconf = this.opcion.NewConfirmed;
-      this.nuevasmuertes = this.opcion.NewDeaths;
-      this.nuevosrec = this.opcion.NewRecovered;
+
       
       let registro = {};
-      registro['id'] = this.nombrepais + " - " + this.fecha_aux;
-      registro['pais'] = this.nombrepais;
-      registro['casostotales'] = this.casostotales;
-      registro['muertestotales'] = this.muertestotales;
-      registro['rectotales'] = this.rectotales;
-      registro['nuevosconf'] = this.nuevosconf;
-      registro['nuevasmuertes'] = this.nuevasmuertes;
-      registro['nuevosrec'] = this.nuevosrec;
+      registro['id'] = this.opcion.Country + " - " + this.fecha_aux;
+      registro['pais'] = this.opcion.Country;
+      registro['casostotales'] = this.opcion.TotalConfirmed;
+      registro['muertestotales'] = this.opcion.TotalDeaths;
+      registro['rectotales'] = this.opcion.TotalRecovered;
+      registro['nuevosconf'] = this.opcion.NewConfirmed;
+      registro['nuevasmuertes'] = this.opcion.NewDeaths;
+      registro['nuevosrec'] = this.opcion.NewRecovered;
       registro['fecha'] = this.fecha_aux;
       console.log(registro);
-      let id = this.nombrepais + " - " + this.fecha_aux;
+      let id = this.opcion.Country + " - " + this.fecha_aux;
       this.dbservice.insertarReg(registro,id).then(res => {
         console.log("Insertado correctamente");
       }).catch(error => {
         console.log(error);
       });
 
-
+        
     }
      
+   }
+   consultaHistorica(){
+     if(this.opcionHistorica!=null||this.opcionHistorica!=undefined){
+       console.log(this.opcionHistorica);
+     }
    }
 
   ngOnInit(): void {
